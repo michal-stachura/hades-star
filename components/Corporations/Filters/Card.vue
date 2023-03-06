e4bc200856f97bfe76d085c0819421fafd49c2d8
Michal Stachura <michal.stachura@santri.pl>
2023-03-06 21:00:55 +0100

    wip: filtering members

--- components/Corporations/Filters/Card.vue
+++ components/Corporations/Filters/Card.vue
@@ -1,6 +1,61 @@
 <script setup lang="ts">
+import { faFilterList } from '@fortawesome/pro-duotone-svg-icons';
+
   const route = useRoute()
   const { corporation } = useCorporationDetails();
+
+  const clickedFilters = ref<string[]>([])
+
+  function toggleFilter(filterId: string): void {
+    if (clickedFilters.value.includes(filterId)) {
+      clickedFilters.value.splice(clickedFilters.value.indexOf(filterId), 1)
+    } else {
+      clickedFilters.value.push(filterId)
+    }
+    filterMembers()
+  }
+
+  function attributeGroup(filterId: string): string {
+    const weaponIds = ['BATTERY', 'LASER', 'MASS_BATTERY', 'DUAL_LASER', 'BARRAGE', 'DART_LAUNCHER'];
+    const shieldIds = ['DELTA_SHIELD', 'PASSIVE_SHIELD', 'OMEGA_SHIELD', 'MIRROR_SHIELD', 'BLAST_SHIELD', 'AREA_SHIELD'];
+    const miningIds = ["MINING_BOOST", "HYDROGEN_BAY_EXTENSION", "ENRICH", "REMOTE_MINING", "HYDROGEN_UPLOAD", "MINING_UNITY","CRUNCH", "GENESIS", "HYDROGEN_ROCKET", "MINING_DRONE"];
+    const supportIds = ["EMP", "TELEPORT", "RED_STAR_LIFE_EXTENDER", "REMOTE_REPAIR", "TIME_WRAP", "UNITY", "SANCTUARY", "STEALTH", "FORTIFY", "IMPULSE", "ALPHA_ROCKET","SALVAGE", "SUPRESS", "DESTINY", "BARRIER", "VENEGANCE", "DELTA_ROCKET", "LEAP", "BOND", "LASER_TURRET", "ALPHA_DRONE", "SUSPEND", "OMEGA_ROCKET", "REMOTE_BOMB"];
+    const tradeIds = ["CARGO_BAY_EXTENSION", "SHIPMENT_COMPUTER", "TRADE_BOOST", "RUSH", "TRADE_BURST", "SHIPMENT_DRONE", "OFFLOAD", "SHIPMENT_BEAM", "ENTRUST", "DISPATCH", "RECALL", "RELIC_DRONE"];
+    // console.log(filterId)
+    if (weaponIds.includes(filterId)) {
+      return 'weapon'
+    } else if (shieldIds.includes(filterId)) {
+      return 'shield'
+    } else if (miningIds.includes(filterId)) {
+      return 'mining'
+    } else if (supportIds.includes(filterId)) {
+      return 'support'
+    } else if (tradeIds.includes(filterId)) {
+      return 'trade'
+    } else {
+      return ''
+    }
+
+  }
+
+  function filterMembers(): void {
+    if (corporation.value) {
+      const chosenFilters = corporation.value.filters.filter(filter => clickedFilters.value.includes(filter.id))
+
+      chosenFilters.forEach(filter => {
+        filter.conditions.forEach(condition => {
+          const attrGroup = attributeGroup(condition.id)
+          console.log(attrGroup)
+
+          // TODO: filter members with conditions and add thenm to finall list of members
+                    
+        })
+        console.log(filter)
+      })
+      // console.log(chosenFilters)
+      // console.log(corporation.value.members)
+    }
+  }
 </script>
 
 
@@ -18,8 +73,9 @@
           :id="filter.id"
           :text="filter.name.toString()"
           :size="'sm'"
-          :layout="'transparent'"
+          :layout="clickedFilters.includes(filter.id) ? '' : 'transparent'"
           :class="'mr-2'"
+          @click="toggleFilter(filter.id)"
         />
     </span>
       <NuxtLink 
--- types/filter.ts
+++ types/filter.ts
@@ -1,7 +1,15 @@
+export interface Condition {
+  id: string;
+  max: number;
+  name: string;
+  set: number;
+  type: string;
+}
+
 export interface Filter {
   id: string;
   name: String;
   corporation: String;
   createdBy: String;
-  conditions: Object;
+  conditions: Condition[];
 }
